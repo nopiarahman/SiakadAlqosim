135cadae10e53bc62c785a786844cdcc91840b61
nopiarahman <nopiarahman93@gmail.com>
2023-06-25 03:11:28 +0700

    tambah template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,7 +27,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title>Login Basic - Pages | Sneat - Bootstrap 5 HTML Admin Template - Pro</title>
+    <title>Login </title>
 
     <meta name="description" content="" />
 
@@ -61,7 +61,7 @@
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="template/js/config.js"></script>
+    <script src="{{ asset('template/js/config.js') }}"></script>
   </head>
 
   <body>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,26 +1,10 @@
-<x-app-layout>
+@extends('layouts.tema')
+@section('content')
+@endsection
+{{-- <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}" x-data>
-                    @csrf
-
-                    <button type="submit">Logout</button>
-                </form>
-                @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                    <x-jet-dropdown-link href="{{ route('api-tokens.index') }}">
-                        {{ __('API Tokens') }}
-                    </x-jet-dropdown-link>
-                @endif
-            </div>
-            <a href="/deleteToken">Delete Token</a>
-        </div>
-</x-app-layout>
+</x-app-layout> --}}
