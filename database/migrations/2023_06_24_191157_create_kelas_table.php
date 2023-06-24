5443e7fd3c21fbc2bca77e9bf9139d1c9dd1836a
nopiarahman <nopiarahman93@gmail.com>
2023-06-25 02:21:47 +0700

    model marhalah dan kelas

---
+++ database/migrations/2023_06_24_191157_create_kelas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Marhalah;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Marhalah::class)->onDelete('CASCADE');
+            $table->string('nama');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
