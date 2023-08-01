d8cd19d5a66cda7db425830a5b2a6267cb87f1c5
nopiarahman <nopiarahman93@gmail.com>
2023-08-02 01:15:46 +0700

    fix login

--- app/Http/Middleware/RestrictApiUsers.php
+++ app/Http/Middleware/RestrictApiUsers.php
@@ -17,7 +17,7 @@ class RestrictApiUsers
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user() && !Auth::user()->hasRole('admin') || !Auth::user()->hasRole('Super-Admin')) {
+        if (!Auth::user()->hasRole('admin') || !Auth::user()->hasRole('Super-Admin')) {
             Auth::user()->logout(); // Force logout the user
             return redirect('/login')->with('error', 'API users are not allowed to access the web interface.');
         }
