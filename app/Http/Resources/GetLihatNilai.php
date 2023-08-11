571988fea4354e36dbb54c90740595f610082c2e
nopiarahman <nopiarahman93@gmail.com>
2023-08-11 15:03:32 +0700

    update respon lihat tugas

---
+++ app/Http/Resources/GetLihatNilai.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GetLihatNilai extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'=>$this->id,
+            'namaTugas'=>$this->tugasTahfidz->namaTugas,
+            'jenisSurah'=>$this->tugasTahfidz->jenisSurah,
+            'nilai'=>$this->nilai,
+        ];
+    }
+}
