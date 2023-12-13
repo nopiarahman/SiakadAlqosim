85f81c0e047890da838b7275d50d89dcda0c79da
nopiarahman <nopiarahman93@gmail.com>
2023-12-13 11:59:32 +0700

    sorted pas

--- app/Http/Controllers/RaportController.php
+++ app/Http/Controllers/RaportController.php
@@ -10,16 +10,22 @@ use App\Models\DataRaportK13;
 class RaportController extends Controller
 {
     function semesterk13(Santri $santri, $kelas) {
-        $nilaiPengetahuan = NilaiK13::select('id', 'mapel_id','PTS','PAS', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8','kelas_id','periode_id')
+        $nilaiPengetahuanAll = NilaiK13::select('id', 'mapel_id','PTS','PAS', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8','kelas_id','periode_id')
         ->where('santri_id',$santri->id)
         ->where('kelas_id',$kelas)
         ->where('periode_id',getPeriodeAktif()->id)
         ->get();
-        $nilaiKeterampilan = NilaiK13::select('id', 'mapel_id', 'k1', 'k2', 'k3', 'k4', 'k5', 'k6', 'k7', 'k8','kelas_id','periode_id')
+        $nilaiKeterampilanAll = NilaiK13::select('id', 'mapel_id', 'k1', 'k2', 'k3', 'k4', 'k5', 'k6', 'k7', 'k8','kelas_id','periode_id')
         ->where('santri_id',$santri->id)
         ->where('kelas_id',$kelas)
         ->where('periode_id',getPeriodeAktif()->id)
         ->get();
+        $nilaiPengetahuan = $nilaiPengetahuanAll->sortBy(function ($nilaiK13) {
+            return $nilaiK13->mapel->nama;
+        });
+        $nilaiKeterampilan = $nilaiKeterampilanAll->sortBy(function ($nilaiK13) {
+            return $nilaiK13->mapel->nama;
+        });
         $dataRaport=DataRaportK13::where('santri_id',$santri->id)->where('periode_id',getPeriodeAktif()->id)->first();
         return view('raport.pas',compact('santri','nilaiPengetahuan','nilaiKeterampilan','dataRaport'));
     }
